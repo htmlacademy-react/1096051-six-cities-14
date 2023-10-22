4cc7aa0c50b58d054ee0c756a209283c9414f9f9
problemon <danilets.pasha@mail.ru>
2023-10-23 04:46:28 +1000

    Немного разбил типы

--- src/types/OfferData.type.ts
+++ src/types/OfferData.type.ts
@@ -5,27 +5,27 @@ export type HostType = {
   name: string;
 }
 
+type Location = {
+  latitude: number;
+  longitude: number;
+  zoom: number;
+}
+
+type City = {
+  location: Location;
+  name: string;
+}
+
 export type OfferDataType = {
   bedrooms: number;
-  city: {
-    location: {
-      latitude: number;
-      longitude: number;
-      zoom: number;
-    };
-    name: string;
-  };
+  city: City;
   description: string;
   goods: string[];
   host: HostType;
   images: string[];
   isFavorite: boolean;
   isPremium: boolean;
-  location: {
-    latitude: number;
-    longitude: number;
-    zoom: number;
-  };
+  location: Location;
   maxAdults: number;
   previewImage: string;
   price: number;
