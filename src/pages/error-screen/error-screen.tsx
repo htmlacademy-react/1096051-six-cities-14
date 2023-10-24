cae69f6de1373e21ce27da01085dfe11062aaccf
problemon <danilets.pasha@mail.ru>
2023-10-24 17:40:34 +1000

    Css module

--- src/pages/error-screen/error-screen.tsx
+++ src/pages/error-screen/error-screen.tsx
@@ -1,29 +1,15 @@
 import { Link } from 'react-router-dom';
 import { PagePaths } from '../../const';
 import { Helmet } from 'react-helmet-async';
+import styles from './error-screen.module.css';
 
 function ErrorScreen(): JSX.Element {
   return (
     <main className="page__main page__main--error">
       <Helmet title="404"></Helmet>
-      <h1 className="page__title" style={
-        {
-          textAlign: 'center'
-        }
-      }
-      >Error 404. Page not found.
+      <h1 className={`page__title ${styles.title}`}>Error 404. Page not found.
       </h1>
-      <Link to={PagePaths.MAIN} style={
-        {
-          display: 'block',
-
-          fontSize: '30px',
-          textAlign: 'center',
-          textDecoration: 'underline',
-          color: '#5FB6FF',
-        }
-      }
-      >Main page
+      <Link to={PagePaths.MAIN} className={ styles.link }> Main page
       </Link>
     </main>
   );
